3dc6a34615481323c9aeb7502f9b086281f79bc1
DutchyD <contactdutchy@gmail.com>
2018-07-15 17:49:04 +0200

    Add Main class, pom and iml

---
+++ Spaggot/src/main/java/com/temporary/helix/Spaggot.java
@@ -0,0 +1,22 @@
+package com.temporary.helix;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * This file is part of Helix
+ * Creation date: 7/15/2018
+ * Created by: Dylan 'Dutchy' de Beer
+ */
+
+public class Spaggot extends JavaPlugin  {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
